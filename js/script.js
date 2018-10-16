fa849c20cb373b9727a67a82c3ed7bfc2f247859
Stas <39906128+RomanovStas@users.noreply.github.com>
2018-10-16 16:15:44 +0300

    Небольшие правки в форме, подключили скрипт
    Вывод русских слов в форме.
    В скрипте прописали функцию для открытия бургер меню(добавление класса responsive для меню с классом topnav)
    бургер меню при уеньшении выводит кривенько(вынести из потока)

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var burgerMenu = document.getElementById('burger-menu');﻿
+burgerMenu.onclick = function myFunction(){
+	var x = document.getElementById("myTopnav")
+
+	if(x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
